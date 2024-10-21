b2b8bb327a00a6771ecb6e56ededf8057e25ae37
AryanPatankar27 <141947771+AryanPatankar27@users.noreply.github.com>
2024-10-21 22:41:13 +0530

    Add files via upload

---
+++ 1.career guidance blog/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomePage from './components/HomePage'; // Import the HomePage component
+
+function App() {
+  return (
+    <div className="App">
+      {/* Render the HomePage */}
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
